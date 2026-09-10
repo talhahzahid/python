c2d77cef0568a8ed4e30496fd1ea01f258194cc7
Talhah Zahid <talhahzahiddev17@gmail.com>
2026-09-10 15:49:02 +0500

    mutable&immutable practice in python

---
+++ mutable_immutable/index.py
@@ -0,0 +1,19 @@
+print("mutable_immutable")
+
+
+username = "John Doe"
+
+print(username)
+
+username = "Jane Smith"
+print(username)
+
+
+x = 10
+y = x
+print("x:", x)
+print("y:", y)
+x = 15
+print("After changing x:")
+print("x:", x)
+print("y:", y)

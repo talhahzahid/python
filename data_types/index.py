19478819d26614c09002b23f77fee938b8daf90c
Talhah Zahid <talhahzahiddev17@gmail.com>
2026-09-25 16:50:28 +0500

    practice string in python

--- data_types/index.py
+++ data_types/index.py
@@ -10,4 +10,39 @@
 a = 5
 b = 2
 a = a + 2
-print(a)
+# print(a)
+
+
+# numbers
+
+
+# string in python
+
+coffee = "Cold Coffee"
+print(coffee[0])
+print(coffee[0:5])
+print(coffee[5:14])
+soft_drinks = "  coca cola  "
+print(soft_drinks.strip())
+print(coffee.replace("Cold", "Hot"))
+print(coffee.find("Coffee"))
+print(coffee.split())
+fruits = "apple, mango, orange"
+print(fruits.split(", "))
+array_num = ["1", "2", "2", "3", "3", "3", "4", "5"]
+print(", ".join(array_num))
+print(array_num.count("3"))
+food = "pizza"
+quantity = 2
+order = "i ordered {} spicy {}"
+print(order.format(quantity, food))
+him = 'he said "pizza is so tasty" '
+print(him)
+text = "hello\nworld"
+text1 = r"hello\nworld"
+print(text)
+print(text1)
+
+
+numbers = "0123456789"
+print(numbers[0:6:2])
